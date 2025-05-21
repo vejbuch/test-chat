0479bcc3a28730d83154809901cba04979e0d72c
Nevim :) <vejbuch2@centrum.cz>
2025-05-21 12:45:05 +0200

    Create cars.ts

---
+++ data/cars.ts
@@ -0,0 +1,32 @@
+// data/cars.ts
+const cars = [
+  {
+    id: 1,
+    make: "Kia",
+    model: "Tasman",
+    year: 2025,
+    color: "Green",
+    price: 20000,
+    image: "/cars/tasman.jpg",
+  },
+  {
+    id: 2,
+    make: "Kia",
+    model: "EV6",
+    year: 2025,
+    color: "Gray",
+    price: 22000,
+    image: "/cars/ev6.jpg",
+  },
+  {
+    id: 3,
+    make: "Kia",
+    model: "EV9",
+    year: 2025,
+    color: "Blue",
+    price: 18000,
+    image: "/cars/ev9.jpg",
+  },
+];
+
+export default cars;
